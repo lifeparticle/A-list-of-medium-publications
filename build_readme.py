1cf63058d6721128eff294677295c31c6bae16a0
Mahbub Zaman <mahbubzaman17@gmail.com>
2020-12-28 22:53:26 +1100

    Create build_readme.py

---
+++ build_readme.py
@@ -0,0 +1,12 @@
+pub_id_dict = {
+	"onezero": 444d13b52878,
+	"human_parts": 544c7006046e,
+	"elemental": 8d6b8a439e32,
+	"forge": 3f6ecf56618,
+	"gen": ae2a65f35510,
+	"marker": 7b6769f2748b,
+	"modus": cb8577c9149e
+	"heated": 1285ba81cada,
+	"zora": 8ccfed20cbb2,
+	"level": 88d9857e584e
+}
